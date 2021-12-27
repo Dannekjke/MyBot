9bfc075562659e318269e1b67c89f450ec0fb1f4
Dannekjke <89574332+Dannekjke@users.noreply.github.com>
2021-12-27 21:05:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+
+import telebot
+
+from telebot import types
+bot = telebot.TeleBot('5062120763:AAEdynX2N7Rh2w1712-3AXIm0ONOxaWX_Hc')
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    sti = open('static/welcome.webp', 'rb')
+    bot.send_sticker(message.chat.id, sti)
+    markup= types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Петух")
+    item2 = types.KeyboardButton("Кабан")
+    item3 = types.KeyboardButton("Конь")
+ 
+    markup.add(item1, item2, item3)
+ 
+    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, И у меня есть для тебя подрок! Ответь на один вопрос: Какое у Лехи любимое животное".format(message.from_user, bot.get_me()),
+        parse_mode='html', reply_markup=markup)
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    if message.chat.type == 'private':
+        if message.text == 'Кабан':
+            bot.send_message(message.chat.id, 'Не верно')
+
+        elif message.text == "Петух":
+            bot.send_message(message.chat.id, "Здесь будет подрок")
+
+        elif message.text == "Кабан":
+            bot.send_message(message.chat.id, 'Не верно')
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="",
+                reply_markup=None)
+ 
+            bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
+                text="")
+ 
+    except Exception as e:
+        print(repr(e))
+ 
+bot.polling(none_stop=True)
